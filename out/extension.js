b29b19345dca7d6a8abff28963c800deb9278e0d
koob <hidamari4256@gmail.com>
2020-09-05 16:57:48 +0900

    コンテキストメニューにコマンド追加
    エディタ上、エクスプローラー上から右クリックで表示されるメニュー内に実行コマンドが表示される対応

--- out/extension.js
+++ out/extension.js
@@ -8,19 +8,17 @@ const child_process_1 = require("child_process");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "cobol-sourceanalysisextention" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('cobol-sourceanalysisextention.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand('cobol-sourceanalysisextention.sourceAnalysis', () => {
         let editor = vscode.window.activeTextEditor; // エディタ取得
         let fileName = editor === null || editor === void 0 ? void 0 : editor.document.fileName; // ファイル名称取得
         var dir_home = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
         var dir_desktop = require("path").join(dir_home, "Desktop");
         const EXE_FILE = dir_desktop + "\\ソース解析ツール\\ExecCobolAnalysis\\ExecCobolAnalysis.exe";
         child_process_1.exec(EXE_FILE + " " + fileName, (error, stdout, stderr) => {
+            if (error)
+                vscode.window.showErrorMessage("エラーが発生しました。詳細はログファイルをご確認ください。");
+            else
+                vscode.window.showInformationMessage("処理正常終了。");
             console.log(stdout);
             console.log(stderr);
         });
--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,7 @@ import { exec } from 'child_process';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cobol-sourceanalysisextention" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cobol-sourceanalysisextention.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('cobol-sourceanalysisextention.sourceAnalysis', () => {
 		let editor = vscode.window.activeTextEditor; // エディタ取得
 		let fileName = editor?.document.fileName; // ファイル名称取得
 
@@ -23,6 +16,11 @@ export function activate(context: vscode.ExtensionContext) {
 		const EXE_FILE = dir_desktop + "\\ソース解析ツール\\ExecCobolAnalysis\\ExecCobolAnalysis.exe";
 		exec(EXE_FILE + " " + fileName, (error,stdout,stderr) =>
 		{
+			if(error)
+				vscode.window.showErrorMessage("エラーが発生しました。詳細はログファイルをご確認ください。");
+			else
+				vscode.window.showInformationMessage("処理正常終了。");
+			
 			console.log(stdout);
 			console.log(stderr);
 		});
